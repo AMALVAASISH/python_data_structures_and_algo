531afdc9499d84c32794962c238ef13a652e7e16
Amalaasish <139550073+AMALVAASISH@users.noreply.github.com>
2025-07-14 17:34:49 +0530

    started the basics

---
+++ basics_brocode/basics.py
@@ -0,0 +1,32 @@
+# multiple assignment in pyton
+
+
+# x,y,z = "orange","apple","mango"
+# print(z)
+
+
+# unpacking a collection
+# coll = [1,2,4]
+
+# a,b,c = coll
+# print(c)
+
+# string methods
+
+# text = "string is better than text"
+# print(text.capitalize())
+# print(text.upper())
+# print(text.count('string'))
+
+
+# https://www.geeksforgeeks.org/python/python-string-methods/
+
+# mytuple = ('list', 'full')
+# res = '-'.join(mytuple)
+# print(res)
+
+# https://www.geeksforgeeks.org/dsa/last-minute-notes-lmns-data-structures-with-python/
+
+
+name= input("enter your name\n")
+print("u can do it",name)
